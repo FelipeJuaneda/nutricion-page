e9ac0fb3faa5c9e465cf0d12de7ddc50b8da24cc
felipe.juaneda <felipejua9@gmail.com>
2022-02-26 17:23:14 -0300

    contacto formulario casi funca

---
+++ enviar.php
@@ -0,0 +1,21 @@
+<?php
+//llamo a names
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+
+//Datos para el correo
+$destinatario = "felipejua9@gmail.com";
+$asunto = "Contacto desde pagina web";
+
+$carta = "De $nombre \n";
+$carta .= "Correo: $correo \n"
+$carta .= "Telefono: $telefono";
+$carta .= "Mensaje: $mensaje";
+
+//Enviando msje
+mail($destinatario,$asunto, $carta);
+
+?>
