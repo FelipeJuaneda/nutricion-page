58234bbb90c75a7206b7132ba9905f0f5316219a
felipe.juaneda <felipejua9@gmail.com>
2022-02-26 16:53:02 -0300

    contacto formulario todavia no funciona

--- js/main.js
+++ js/main.js
@@ -1,15 +1,68 @@
-const navMenu = () =>{
+const navMenu = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.lista-nav');
     const listaNav = document.querySelector('.lista-na li')
 
-        //Toggle
-    burger.addEventListener('click', ()=>{
+    //Toggle
+    burger.addEventListener('click', () => {
         nav.classList.toggle('nav-activo');
         //Animacion cruz
         burger.classList.toggle('toggle')
     });
-    
+
 };
 
 navMenu();
+
+
+//FORMULARIO
+$(document).ready(function(){
+
+    $('#btnSend').click(function(){
+
+        var errores = '';
+
+        // Validado Nombre ==============================
+        if( $('#names').val() == '' ){
+            errores += '<p>Escriba un nombre</p>';
+            $('#names').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#names').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Correo ==============================
+        if( $('#email').val() == '' ){
+            errores += '<p>Ingrese un correo</p>';
+            $('#email').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#email').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Mensaje ==============================
+        if( $('#mensaje').val() == '' ){
+            errores += '<p>Escriba un mensaje</p>';
+            $('#mensaje').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#mensaje').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // ENVIANDO MENSAJE ============================
+        if( errores == '' == false){
+            var mensajeModal = '<div class="modal_wrap">'+
+                                    '<div class="mensaje_modal">'+
+                                        '<h3>Errores encontrados</h3>'+
+                                        errores+
+                                        '<span id="btnClose">Cerrar</span>'+
+                                    '</div>'+
+                                '</div>'
+
+            $('body').append(mensajeModal);
+        }
+
+        // CERRANDO MODAL ==============================
+        $('#btnClose').click(function(){
+            $('.modal_wrap').remove();
+        });
+    });
+
+});
