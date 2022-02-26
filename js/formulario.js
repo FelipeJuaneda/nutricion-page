4e6d801a19003a2de2197b9e31cd7576474f2a2b
felipe.juaneda <felipejua9@gmail.com>
2022-02-26 19:54:02 -0300

    contacto formulario casi funca

---
+++ js/formulario.js
@@ -0,0 +1,14 @@
+const $form = document.querySelector('#form');
+const $buttonMailto =document.querySelector('#trucazo')
+
+
+$form.addEventListener('submit', handleSubmit)
+console.log($form);
+function handleSubmit(event) {
+    event.prevenDefault();
+    const form = new FormData(this);
+    $buttonMailto.setAttribute(`href','mailto:felipejua9@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('mensaje')}`)
+
+    $buttonMailto.click()
+
+}
