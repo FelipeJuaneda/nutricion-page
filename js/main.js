b73684be2d7ae368a6657588e4fa1c8708486bce
felipe.juaneda <felipejua9@gmail.com>
2022-03-01 22:23:09 -0300

    Contacto casdi terminada

--- js/main.js
+++ js/main.js
@@ -15,54 +15,71 @@ const navMenu = () => {
 navMenu();
 
 
-//FORMULARIO
-$(document).ready(function(){
 
-    $('#btnSend').click(function(){
+$(document).ready(function () {
+
+    $('#btnSend').click(function () {
 
         var errores = '';
 
         // Validado Nombre ==============================
-        if( $('#names').val() == '' ){
+        if ($('#names').val() == '') {
             errores += '<p>Escriba un nombre</p>';
             $('#names').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#names').css("border-bottom-color", "#d1d1d1")
         }
 
         // Validado Correo ==============================
-        if( $('#email').val() == '' ){
+        if ($('#email').val() == '') {
             errores += '<p>Ingrese un correo</p>';
             $('#email').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#email').css("border-bottom-color", "#d1d1d1")
         }
 
         // Validado Mensaje ==============================
-        if( $('#mensaje').val() == '' ){
+        if ($('#mensaje').val() == '') {
             errores += '<p>Escriba un mensaje</p>';
             $('#mensaje').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#mensaje').css("border-bottom-color", "#d1d1d1")
         }
 
         // ENVIANDO MENSAJE ============================
-        if( errores == '' == false){
-            var mensajeModal = '<div class="modal_wrap">'+
-                                    '<div class="mensaje_modal">'+
-                                        '<h3>Errores encontrados</h3>'+
-                                        errores+
-                                        '<span id="btnClose">Cerrar</span>'+
-                                    '</div>'+
-                                '</div>'
+        if (errores == '' == false) {
+            var mensajeModal = '<div class="modal_wrap">' +
+                '<div class="mensaje_modal">' +
+                '<h3>Errores encontrados</h3>' +
+                errores +
+                '<span id="btnClose">Cerrar</span>' +
+                '</div>' +
+                '</div>'
 
             $('body').append(mensajeModal);
         }
 
         // CERRANDO MODAL ==============================
-        $('#btnClose').click(function(){
+        $('#btnClose').click(function () {
             $('.modal_wrap').remove();
         });
     });
 
 });
+
+
+
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "felipejua9@gmail.com",
+        Password: "250C9D02B73B098FB955A40C954B3F84FB6F",
+        To: 'felipejua9@gmail.com',
+        From: document.getElementById("email").value,
+        Subject: "Necesito Nutricionista!",
+        Body: "Nombre: " + document.getElementById("names").value + "<br> Email: " + document.getElementById("email").value + "<br> Telefono: " + document.getElementById("phone").value + "<br> Mensaje: " + document.getElementById("mensaje").value
+    }).then(
+        message => alert("Enviado correctamente!")
+    );
+}
